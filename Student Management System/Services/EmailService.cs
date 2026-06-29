7fb6c9be584e68b721deee938af7de28b70af53f
Dipen-Limbu <limbudipen518@gmail.com>
2026-06-29 15:01:06 +0545

    user secret has been done

--- Student Management System/Services/EmailService.cs
+++ Student Management System/Services/EmailService.cs
@@ -1,6 +1,42 @@
-﻿namespace Student_Management_System.Services
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Student_Management_System.Services
 {
     public class EmailService
     {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void SendEmail(string toEmail, string subject, string body)
+        {
+            var email = _config["EmailSettings:Email"];
+            var password = _config["EmailSettings:AppPassword"];
+            var host = _config["EmailSettings:Host"];
+            var port = int.Parse(_config["EmailSettings:Port"]);
+
+            var client = new SmtpClient(host)
+            {
+                Port = port,
+                Credentials = new NetworkCredential(email, password),
+                EnableSsl = true
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(email),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(toEmail);
+
+            client.Send(mailMessage);
+        }
     }
 }
